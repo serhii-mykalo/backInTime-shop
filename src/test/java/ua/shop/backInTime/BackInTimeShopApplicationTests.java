ef37d85f0a02af12fc4889dc2e4ee307c6d2d7a2
Serhii Mykalo <serhii.mask@gmail.com>
2024-01-25 11:59:40 +0200

    init: start pet-project "Back In Time" shop

---
+++ src/test/java/ua/shop/backInTime/BackInTimeShopApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.shop.backInTime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackInTimeShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
